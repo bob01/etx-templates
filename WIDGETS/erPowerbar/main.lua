7c14451b26194bf0ccaf7ba2bd79eb21a5d03b41
Rob Gayle <bob01@rogers.com>
2026-03-19 00:43:17 -0400

    TMPL-00024 - powerbar - bar paint from ethos bar

--- WIDGETS/erPowerbar/main.lua
+++ WIDGETS/erPowerbar/main.lua
@@ -42,7 +42,7 @@
 -- friendlier UI, new name (vPowerBar), specify cell count option, reserve, haptic critical
 -- Author: Rob Gayle (bob00@rogers.com)
 -- Date: 2026
--- ver: 0.9.0.03180
+-- ver: 0.9.0.03181
 
 local app_name = "erPowerbar"
 
@@ -54,6 +54,7 @@ local BAR_COLOR_LOW         = lcd.RGB(0xff, 0xff, 0x00)
 local BAR_COLOR_CRITICAL    = lcd.RGB(0xff, 0x00, 0x00)
 local BAR_COLOR_CHECK       = lcd.RGB(0xb8, 0xb8, 0xb8)
 local BAR_COLOR_BACKGROUND  = lcd.RGB(0xc8, 0xc8, 0xc8)
+local BAR_COLOR_LINE        = lcd.RGB(160, 160, 160)
 
 local cellFull = 4.16
 
@@ -161,7 +162,7 @@ local function create(zone, options)
         border_b = 10,
 
         isTelemetryActive = 0,
-        vPercent = 0,
+        fuel = 0,
         vReserve = 20,
         vLow = 10,
         vMah = 0,
@@ -170,7 +171,7 @@ local function create(zone, options)
         low_batt_blink = 0,
         voltTimer = VOLTTIMER_DISABLED,
         mainValue = 0,
-        secondaryValue = 0,
+        volts = 0,
 
         battNextPlay = 0,
         battPercentPlayed = 100,
@@ -243,19 +244,19 @@ local function calculateBatteryData(wgt)
 
     -- battery voltage
     wgt.mainValue = v / wgt.cellCount
-    wgt.secondaryValue = v
+    wgt.volts = v
 
     -- battery percentage
     if wgt.sensorPcntId ~= 0 then
         local pcnt = getValue(wgt.sensorPcntId)
         if pcnt < wgt.vReserve then
-            wgt.vPercent = pcnt - wgt.vReserve
+            wgt.fuel = pcnt - wgt.vReserve
         else
             local usable = 100 - wgt.vReserve
-            wgt.vPercent = (pcnt - wgt.vReserve) / usable * 100
+            wgt.fuel = (pcnt - wgt.vReserve) / usable * 100
         end
     else
-        wgt.vPercent = 0
+        wgt.fuel = 0
     end
 
     -- battery mah
@@ -266,12 +267,12 @@ end
 
 
 -- color for gauge
-local function getPercentColor(wgt)
+local function getBarColor(wgt)
     local critical = wgt:getCritical()
-    if wgt.vPercent <= critical then
+    if wgt.fuel <= critical then
         -- red
         return BAR_COLOR_CRITICAL
-    elseif wgt.vPercent <= critical + 20 then
+    elseif wgt.fuel <= critical + 20 then
         -- yellow
         return BAR_COLOR_LOW
     else
@@ -285,10 +286,27 @@ local function refreshZoneSmall(wgt)
     local myBatt = { ["x"] = 4, ["y"] = 4, ["w"] = wgt.zone.w - 8, ["h"] = wgt.zone.h - 8, ["segments_w"] = 25, ["color"] = WHITE, ["cath_w"] = 6, ["cath_h"] = 20 }
 
     -- fill battery
-    local fill_color = getPercentColor(wgt)
-    lcd.drawGauge(myBatt.x, myBatt.y, myBatt.w, myBatt.h, wgt.vPercent, 100, fill_color)
+    -- local fill_color = getBarColor(wgt)
+    -- lcd.drawGauge(myBatt.x, myBatt.y, myBatt.w, myBatt.h, wgt.fuel, 100, fill_color)
 
-    -- draw battery
+    -- background
+    local color = BAR_COLOR_BACKGROUND
+    lcd.drawFilledRectangle(myBatt.x, myBatt.y, myBatt.w, myBatt.h, color)
+
+    -- bar
+    if wgt.fuel and wgt.volts and wgt.volts > 0 then
+        local fill
+        fill = wgt.fuel > 0 and wgt.fuel or 100
+
+        local bar_width = math.floor((((myBatt.w - 2) / 100) * fill) + 2)
+        color = getBarColor(wgt)
+        lcd.drawFilledRectangle(myBatt.x, myBatt.y, bar_width, myBatt.h, color)
+
+        color = BAR_COLOR_LINE
+        lcd.drawLine(myBatt.x + bar_width, myBatt.y, myBatt.x + bar_width, myBatt.y + myBatt.h, SOLID, color)
+    end
+
+    -- outline
     lcd.drawRectangle(myBatt.x, myBatt.y, myBatt.w + 1, myBatt.h, wgt.text_color, 2)
 
     -- write text
@@ -298,10 +316,10 @@ local function refreshZoneSmall(wgt)
     local volts
     if wgt.cell_detected then
         -- cell count available
-        volts = string.format("%.1f v / %.2f v (%.0fs)", wgt.secondaryValue, wgt.mainValue, wgt.cellCount);
+        volts = string.format("%.1f v / %.2f v (%.0fs)", wgt.volts, wgt.mainValue, wgt.cellCount);
     else
         -- cell count not available
-        volts = string.format("%.1f v / %.2f v (?s)", wgt.secondaryValue, wgt.mainValue);
+        volts = string.format("%.1f v / %.2f v (?s)", wgt.volts, wgt.mainValue);
     end
     lcd.drawText(myBatt.x + 8, myBatt.y + 4, volts, BOLD + LEFT  + wgt.text_color + low_batt_blink)
 
@@ -310,7 +328,7 @@ local function refreshZoneSmall(wgt)
         lcd.drawText(myBatt.x + 8, myBatt.y + myBatt.h / 2, mah, BOLD + LEFT  + wgt.text_color)
     end
 
-    local percent = string.format("%.0f%%", wgt.vPercent)
+    local percent = string.format("%.0f%%", wgt.fuel)
     lcd.drawText(myBatt.x + myBatt.w - 4, myBatt.y + myBatt.h / 2, percent, BOLD + VCENTER + RIGHT + MIDSIZE + wgt.text_color + low_batt_blink)
 end
 
@@ -343,7 +361,7 @@ local function background(wgt)
     calculateBatteryData(wgt)
 
     -- voice alerts
-    local fvpcnt = wgt.vPercent
+    local fvpcnt = wgt.fuel
 
     local critical = wgt:getCritical()
 
